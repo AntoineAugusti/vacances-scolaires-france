21aa3c6402ed951094a049ec1c39f6338f0e7420
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-10-01 17:52:43 +0200

    Add more tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -6,6 +6,8 @@ from vacances_scolaires_france import Dates
 
 
 class TestInit(unittest.TestCase):
+    EXPECTED_KEYS = ['date', 'nom_vacances', 'vacances_zone_a', 'vacances_zone_b', 'vacances_zone_c']
+
     def test_is_holiday(self):
         d = Dates()
 
@@ -30,7 +32,37 @@ class TestInit(unittest.TestCase):
         self.assertEquals(len(res), 151)
 
         for k, v in res.items():
-            self.assertEquals(sorted(v.keys()), ['date', 'nom_vacances', 'vacances_zone_a', 'vacances_zone_b', 'vacances_zone_c'])
+            self.assertEquals(sorted(v.keys()), self.EXPECTED_KEYS)
 
         self.assertEquals({}, d.holidays_for_year(2020))
 
+    def test_holiday_for_year_by_name(self):
+        d = Dates()
+
+        res = d.holiday_for_year_by_name(2017, 'Vacances de la Toussaint')
+
+        self.assertEquals(len(res), 16)
+        for k, v in res.items():
+            self.assertEquals(sorted(v.keys()), self.EXPECTED_KEYS)
+        self.assertEquals(
+            sorted([v['date'] for v in res.values()]),
+            [
+                '2017-10-21', '2017-10-22', '2017-10-23', '2017-10-24',
+                '2017-10-25', '2017-10-26', '2017-10-27', '2017-10-28',
+                '2017-10-29', '2017-10-30', '2017-10-31', '2017-11-01',
+                '2017-11-02', '2017-11-03', '2017-11-04', '2017-11-05'
+            ]
+        )
+
+        self.assertEquals({}, d.holiday_for_year_by_name(2017, 'Foo'))
+
+    def test_holidays_for_year_and_zone(self):
+        d = Dates()
+
+        res = d.holidays_for_year_and_zone(2017, 'A')
+
+        self.assertEquals(len(res), 120)
+        for k, v in res.items():
+            self.assertEquals(sorted(v.keys()), self.EXPECTED_KEYS)
+
+            self.assertTrue(v['vacances_zone_a'])
