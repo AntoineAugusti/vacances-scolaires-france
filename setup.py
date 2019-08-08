2da6c3e0087b2d86ab91cd5639c8dd01a07bdee3
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-08-08 13:39:05 +0200

    Release 0.6.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="vacances_scolaires_france",
     license="MIT",
     packages=["vacances_scolaires_france"],
-    version="0.6.4",
+    version="0.6.5",
     description="Get school holiday dates for metropolitan France",
     author="Antoine Augusti",
     author_email="hi@antoine-augusti.fr",
